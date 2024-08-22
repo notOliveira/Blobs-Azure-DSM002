c98e538ff2603803211756c8572a5ba7b847d21f
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-08-22 14:04:37 -0300

    Criando entidades com campos personalizados

--- blobcn/views.py
+++ blobcn/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from .services.blob_services import list_blob_urls
 from .services.tables_services import AzureTableService
+from django.http import HttpResponseBadRequest
 import json
 
 table_service = AzureTableService()
@@ -27,15 +28,34 @@ def list_entities_view(request):
 
 # Create entity view
 @csrf_exempt
-@require_http_methods(["POST"])
+@require_http_methods(["POST", "GET"])
 def create_entity_view(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        partition_key = data.get('PartitionKey')
-        row_key = data.get('RowKey')
-        properties = {k: v for k, v in data.items() if k not in ['PartitionKey', 'RowKey']}
-        table_service.create_entity(partition_key, row_key, **properties)
-        return redirect('list_entities')  # Redireciona para a página de listagem após criação
+        try:
+            # Decodifica o JSON vindo no request.body
+            data = json.loads(request.body)
+            
+            # Extrai PartitionKey e RowKey do JSON
+            partition_key = data.get('PartitionKey')
+            row_key = data.get('RowKey')
+            
+            if not partition_key or not row_key:
+                return HttpResponseBadRequest("PartitionKey e RowKey são obrigatórios.")
+            
+            # Extrai as outras propriedades
+            properties = {k: v for k, v in data.items() if k not in ['PartitionKey', 'RowKey']}
+            
+            # Chama o serviço para criar a entidade
+            table_service.create_entity(partition_key, row_key, **properties)
+            
+            # Redireciona para a página de listagem após criação
+            return redirect('list_entities')
+        
+        except json.JSONDecodeError:
+            return HttpResponseBadRequest("Erro ao decodificar o JSON.")
+    
+    # Se o método for GET, renderize o formulário
+    return render(request, 'blobcn/create_entity.html')
 
 # Read entity view
 @require_http_methods(["GET"])
@@ -52,8 +72,6 @@ def update_entity_view(request, partition_key, row_key):
     return redirect('list_entities')  # Redireciona para a página de listagem após atualização
 
 # Delete entity view
-@csrf_exempt
-@require_http_methods(["DELETE"])
 def delete_entity_view(request, partition_key, row_key):
     table_service.delete_entity(partition_key, row_key)
     return redirect('list_entities')  # Redireciona para a página de listagem após deleção
