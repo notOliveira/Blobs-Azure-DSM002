83f2652bc457880e8ce200ce6eac1d1c50e5c8cd
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-08-22 11:02:57 -0300

    Adicionando arquivo base nas telas, colocando em pt-br, organizando melhor a estrutura

--- blobcn/urls.py
+++ blobcn/urls.py
@@ -4,12 +4,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Páginas
     path('', views.index, name='index'),
     path('storage/', views.storage, name='storage'),
-
     path('list-entities/', views.list_entities_view, name='list_entities'),
     path('create-entity/', views.create_entity_view, name='create_entity'),
+
+    # Páginas que dependem de parâmetros
     path('read-entity/<str:partition_key>/<str:row_key>/', views.read_entity_view, name='read_entity'),
+
+    # Páginas que dependem de parâmetros e de métodos HTTP
     path('update-entity/<str:partition_key>/<str:row_key>/', views.update_entity_view, name='update_entity'),
     path('delete-entity/<str:partition_key>/<str:row_key>/', views.delete_entity_view, name='delete_entity'),
 ]
